a929036412a1edc3debd67d1b45731592226b981
James Booth <boothj5@gmail.com>
2011-12-02 02:18:51 +0000

    euler2 uses test to check when to stop

--- lisp/euler2.lisp
+++ lisp/euler2.lisp
@@ -1,8 +1,8 @@
-(defun fib (n)
-    (let ((fiblist '(2 1)))
-        (dotimes (i (- n 2))
-            (setf fiblist 
-                (cons (+ (first fiblist) (second fiblist)) fiblist)))
-        fiblist))
-
-(reduce #'+ (remove-if-not #'evenp (fib 32)))
+(defun fibs ()
+    (let ((fiblist '(2 1)) (stop nil))
+        (loop until stop do
+            (let ((new-fib (+ (first fiblist) (second fiblist))))
+                (when (< new-fib 4000000)   
+                    (setf fiblist (cons new-fib fiblist)))
+                (setf stop (> new-fib 4000000))))
+        (reduce #'+ (remove-if-not #'evenp fiblist))))
