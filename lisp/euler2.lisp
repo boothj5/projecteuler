9fdf5025f18c6414f7e0a1d3ab7e893d282076ee
James Booth <boothj5@gmail.com>
2011-12-11 16:25:17 +0000

    Generalised euler2.lisp

--- lisp/euler2.lisp
+++ lisp/euler2.lisp
@@ -1,8 +1,10 @@
-(defun fibs ()
+(defun sum-even-fibs-below (max)
     (let ((fiblist '(2 1)) (stop nil))
         (loop until stop do
             (let ((new-fib (+ (first fiblist) (second fiblist))))
-                (when (< new-fib 4000000)   
+                (when (< new-fib max)   
                     (setf fiblist (cons new-fib fiblist)))
-                (setf stop (> new-fib 4000000))))
+                (setf stop (> new-fib max))))
         (reduce #'+ (remove-if-not #'evenp fiblist))))
+
+(sum-even-fibs-below 4000000)
